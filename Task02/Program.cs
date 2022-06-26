67d53234ebedccab06c1c2fa22dc5a19231e8703
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-26 14:39:05 +0500

    add Task02 DivideInt

---
+++ Task02/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 2. Даны два числа a, b, такие что a < b. Вывести на экран сколько раз число a поместиться в числе b.
+
+Console.Write("Введите число a: ");
+double a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число b больше числа a: ");
+double b = Convert.ToInt32(Console.ReadLine());
+
+double division = b/a;
+
+if(a<b)
+{
+    Console.WriteLine($"Число a поместится в числе b {(Math.Truncate(division))} раз");
+}
+else
+{
+    Console.WriteLine("Число a должно быть меньше числа b");
+}
