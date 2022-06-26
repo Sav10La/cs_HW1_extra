cd3e83172b0b6f2c3a278cd2edf97fb5390bed3c
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-26 21:31:09 +0500

    add Task03 AscendingOrder

---
+++ Task03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3. На ввод подаётся рост трёх спортсменов. Расположить их от большего к меньшему.
+
+Console.WriteLine("Введите рост 1-ого спортсмена в см: ");
+int height1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите рост 2-ого спортсмена в см: ");
+int height2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите рост 3-ого спортсмена в см: ");
+int height3 = Convert.ToInt32(Console.ReadLine());
+
+int[] myArr = new int[3] {height1, height2, height3};
+int i, j, temp;
+for(i=0;i<3;i++) {
+    Console.Write("{0} ",myArr[i]);
+}
+for(i=0; i<3; i++) {
+    for(j=i+1; j<3; j++) {
+        if(myArr[i] < myArr[j]) {
+            temp = myArr[i];
+            myArr[i] = myArr[j];
+            myArr[j] = temp;
+        }
+    }
+}
+Console.Write("\nDescending order:\n");
+for(i=0; i<3; i++) {
+    Console.Write("{0} ", myArr[i]);
+}
