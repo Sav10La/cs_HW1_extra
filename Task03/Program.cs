bd579970dda2e09d751c0c2a30a716fd1c4139ea
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-26 22:45:00 +0500

    Modified Ascending Order

--- Task03/Program.cs
+++ Task03/Program.cs
@@ -1,29 +1,57 @@
 ﻿// Задача 3. На ввод подаётся рост трёх спортсменов. Расположить их от большего к меньшему.
 
 Console.WriteLine("Введите рост 1-ого спортсмена в см: ");
-int height1 = Convert.ToInt32(Console.ReadLine());
+int a = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите рост 2-ого спортсмена в см: ");
-int height2 = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
 
 Console.WriteLine("Введите рост 3-ого спортсмена в см: ");
-int height3 = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
 
-int[] myArr = new int[3] {height1, height2, height3};
-int i, j, temp;
-for(i=0;i<3;i++) {
-    Console.Write("{0} ",myArr[i]);
-}
-for(i=0; i<3; i++) {
-    for(j=i+1; j<3; j++) {
-        if(myArr[i] < myArr[j]) {
-            temp = myArr[i];
-            myArr[i] = myArr[j];
-            myArr[j] = temp;
+int max, ave, min;
+
+if(a>b)
+{
+    if(a>c)
+    {
+        if(b>c)
+        {
+            max = a;
+            ave = b;
+            min = c;
+        }
+        else
+        {
+            max = a;
+            ave = c;
+            min = b;
         }
     }
+    else
+    {
+        max = c;
+        ave = a;
+        min = b;
+    };
 }
-Console.Write("\nDescending order:\n");
-for(i=0; i<3; i++) {
-    Console.Write("{0} ", myArr[i]);
+else if (a>c)
+{
+    max = b;
+    ave = a;
+    min = c;
 }
+else if (b>c)
+{
+    max = b;
+    ave = c;
+    min = a;
+}
+else
+{
+    max = c;
+    ave = b;
+    min = a;
+}
+
+Console.WriteLine($"{max}, {ave}, {min}");
