3c35ca5179c07802a55184287de1778c0e8554fe
Aziz Sharakhmedov <aziz.sharakhmedov@gmail.com>
2022-06-26 21:39:18 +0500

    add Task04 Seconds

---
+++ Task04/Program.cs
@@ -0,0 +1,5 @@
+﻿// Задача 4. Дано N секунд. Вывести время в формате часы:минуты:секунды.
+
+Console.Write("Введите количество секунд: ");
+int sec = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(TimeSpan.FromSeconds(sec));
